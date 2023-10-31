a7eeda86ca1f86ca51a44054fef5eea90e6d6990
33641 <33641@RJMOM>
2023-10-31 10:25:06 +0100

    Add project files.

---
+++ WebApplication4/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication4
+{
+    public class Customer
+    {
+        public int CustomerId {  get; set; }
+        public string CustomerName { get; set; }
+     
+        public string MobileNumber { get; set; }
+        public string Email { get; set; }
+        
+
+    }
+}
---
+++ WebApplication4/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication4.Data;
+var builder = WebApplication.CreateBuilder(args);
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+var dbUser = Environment.GetEnvironmentVariable("DB_USER");
+var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID={dbUser};Password={dbPassword}";
+builder.Services.AddDbContext<WebApplication4Context>(opt =>
+   opt.UseSqlServer(connectionString));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
