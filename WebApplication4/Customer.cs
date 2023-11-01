a0fada2c93249570404cc99873d67e2becff6e56
NaifGit <149481201+NaifGit@users.noreply.github.com>
2023-11-01 21:36:06 +0100

    Comment added in Customer.cs

--- WebApplication4/Customer.cs
+++ WebApplication4/Customer.cs
@@ -2,10 +2,13 @@
 {
     public class Customer
     {
+        //This is CustomerID
         public int CustomerId {  get; set; }
+        //This is CustomerName
         public string CustomerName { get; set; }
-     
+        //This is Mobile Number
         public string MobileNumber { get; set; }
+        //This is Email
         public string Email { get; set; }
         
 
